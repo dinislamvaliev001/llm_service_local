52149485e898dbb72a780c7c040fa020692657b8
Dinislam Valiev <dinislamvaliev001@gmail.com>
2026-03-22 21:53:46 +0300

    update rag agent

--- src/application/agents/aggregator_agent.py
+++ src/application/agents/aggregator_agent.py
@@ -1,10 +1,26 @@
 # src/application/agents/aggregator_agent.py
 from src.application.workflows.state import ChatState
 
+# def aggregator_node(state: ChatState) -> dict:
+#     rag_docs = state.get("rag_documents", [])
+#     sql_docs = state.get("sql_documents", [])
+
+#     return {
+#         "retrieved_documents": rag_docs + sql_docs
+#     }
+
 def aggregator_node(state: ChatState) -> dict:
     rag_docs = state.get("rag_documents", [])
     sql_docs = state.get("sql_documents", [])
 
+    combined = rag_docs + sql_docs
+
+    # сортировка по score
+    combined.sort(key=lambda x: x.get("score", 0), reverse=True)
+
+    # threshold + top-k
+    filtered = [doc for doc in combined if doc.get("score", 0) >= 0.6][:10]
+
     return {
-        "retrieved_documents": rag_docs + sql_docs
+        "retrieved_documents": filtered
     }
